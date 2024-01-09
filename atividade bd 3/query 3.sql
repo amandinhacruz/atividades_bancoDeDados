d28d95fba6e30afb64be5fedbae540b3a54ff19c
amandinhacruz <amandaellem2023@gmail.com>
2024-01-09 00:11:15 -0300

    subindo uma parte da 3 atividade que faltava

---
+++ atividade bd 3/query 3.sql
@@ -0,0 +1,12 @@
+INSERT INTO tb_estudantes (nome, cursoMatriculado, media, dataMatricula, contato) 
+VALUES 
+("Natalia Oliveira", "Engenharia da Computação", 9.00, "2024-02-10", "natalia_oliveira@email.com, (83) 9878-8765"),
+("Daniela Xavier", "Gestão de Tecnologia da Informação ", 9.00, "2023-12-12", "daniela123@email.com, (81) 9876-0987"),
+("Amanda Cruz", "Engenharia da Computação", 10.00, "2024-02-10", "amandacruz@email.com, (85) 83826-9746"),
+("João da Silva", "Psicologia", 4.00, "2021-10-28", "sila.joao@email.com, (87) 9809-7534"),
+("Marcos Santos", "Engenharia de Pesca", 8.00, "2021-02-10", "santos.marcos@email.com, (85) 9800-2727"),
+("Joana Pereira", "Artes Visuais", 6.00, "2022-02-08", "joanapereira@email.com, (85) 8709-2345"),
+("Matheus Luz", "Ciências Sociais", 8.00, "2020-02-02", "luz.matheus@email.com, (85) 8765-7435"),
+("Ana Scrum", "Jornalismo", 8.00, "2020-02-02", "scrum_ana@email.com, (85) 9090-8765");
+
+SELECT * FROM tb_estudantes;
---
+++ atividade bd 3/query 6.sql
@@ -0,0 +1,4 @@
+
+UPDATE tb_estudantes SET media = 5 WHERE ID = 4;
+
+SELECT * FROM tb_estudantes;
